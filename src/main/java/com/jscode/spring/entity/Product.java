c5c67beb0d2f0d7e9d06ac8c45351c3f1da5f39a
mog-hi <juhe1998@naver.com>
2023-04-05 09:12:53 +0900

    feat: 상품 entity에 상점과의 연관관계 추가

--- src/main/java/com/jscode/spring/entity/Product.java
+++ src/main/java/com/jscode/spring/entity/Product.java
@@ -1,6 +1,7 @@
 package com.jscode.spring.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Entity
 @ToString
 @Getter
+@NoArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +22,17 @@ public class Product {
     @Column
     private Long price;
 
-    public Product() {
+    @ManyToOne
+    @JoinColumn(name="store_id")
+    private Store store;
 
+    public Product(String name, Long price, Store store) {
+        this.name = name;
+        this.price = price;
+        this.store = store;
+    }
+
+    public static Product created(String name, Long price, Store store) {
+        return new Product(name, price, store);
     }
 }
