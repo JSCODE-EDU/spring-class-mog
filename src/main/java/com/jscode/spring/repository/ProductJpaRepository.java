28582c7ae9bddb2e8bc8031ede0f31eca8c198ed
mog-hi <juhe1998@naver.com>
2023-04-05 09:09:21 +0900

    refactor: 기존에 spring data jpa 없이 직접 구현했던 ProductRepository 제거

--- src/main/java/com/jscode/spring/repository/ProductJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jscode.spring.repository;
-
-import com.jscode.spring.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductJpaRepository extends JpaRepository<Product, Long> {
-}
